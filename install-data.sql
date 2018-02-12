177458223dee9a8e16a42842a74da1435fc7f3ac
Konstantin Polyakov <kostyan_93@mail.ru>
2018-02-12 22:52:46 +0300

    install-data.sql was added

---
+++ install-data.sql
@@ -0,0 +1,6 @@
+INSERT INTO `{TABLE_PREFIX}mod_wbs_portal_obj_project_resource_category` (`resource_category_name`) VALUES 
+("Люди"),
+("Электронные компоненты"),
+("Механика"),
+("Оборудование"),
+("Недвижимость");
